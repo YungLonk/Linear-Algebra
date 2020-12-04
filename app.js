bfe94f02ddd555f61024d5d650d91a074d3d8b41
YungLonk <amadeoc72@gmail.com>
2020-12-03 21:43:46 -0500

    basic server

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
+// Modules
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
+// Other variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(logger('dev'));
-app.use(bodyParser);
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+// Middleware (app.use statements)
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// Requests
+app.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, function() {console.log(`Server listing on port ${PORT}.`)});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
