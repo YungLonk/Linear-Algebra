15f37f3df0fef8fbf00a44dfc18a3d9d1d7717d3
YungLonk <amadeoc72@gmail.com>
2020-12-04 16:59:33 -0500

    made public static

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware (app.use statements)
+app.use(express.static(path.join(__dirname, "public")));
 
 // Requests
-app.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
